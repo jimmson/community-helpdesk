12610aaa620f7a038ee6f007aed28ad1cd22136e
jimmy <jimmy@luno.com>
2020-05-12 17:18:49 +0200

    Add font

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,7 +1,13 @@
 import { createGlobalStyle } from "styled-components";
+import SFProDisplayLight from "../fonts/SF-Pro-Display-Light.otf";
 
 export const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'SF Pro Display Light';
+    src: local('SF Pro Display Light'), url(${SFProDisplayLight}) format('opentype');
+  }
   body {
+    font-family: 'SF Pro Display Light';
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
   }
